21ca054612ce2c39e832d1df6ccc8b4e31264750
DamienLouie <h.damien.louie@gmail.com>
2025-08-26 12:53:16 +0800

    MY FIRST JAVA FILE
    MY FIRST JAVA FILE FOR APCSA

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] arg) {
+        System.out.println("Java is a high-level, class-based, object-oriented programming language.");
+
+
+    }
+}
